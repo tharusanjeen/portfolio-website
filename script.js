d73e5089ace005b11cf38aaf568081ad0c9ecb4e
Sanjeen Tharu <167242918+tharusanjeen@users.noreply.github.com>
2024-05-27 16:42:26 +0545

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,7 @@ const navbar = document.querySelector("#navBar");
 
 btn.addEventListener("click", function() {
     navbar.classList.toggle("max-h-[20rem]");
+    btn.children[0].classList.toggle("ri-menu-3-line");
+    btn.children[0].classList.toggle("ri-close-fill");
+    btn.children[0].classList.toggle("text-3xl");
 });
-
-const links = document.querySelectorAll("#navbar li a");
-links.forEach(function(link) {
-    link.addEventListener("click", function() {
-        alert("Hello there");
-    });
-})
